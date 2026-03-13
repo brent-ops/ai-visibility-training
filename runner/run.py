28419797c5453892c3b6f79e8385a7790d4ebaed
Brent <brent@greenline.nyc>
2026-03-12 19:58:54 -0500

    Load keywords from CSV in runner

--- runner/run.py
+++ runner/run.py
@@ -1,7 +1,14 @@
+import csv
 import datetime
 
 print("Runner starting...")
 
 now = datetime.datetime.utcnow()
-
 print(f"Run started at: {now}")
+
+print("Loading keywords...")
+
+with open("data/keywords.csv", newline="") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print("Keyword:", row["keyword"])
